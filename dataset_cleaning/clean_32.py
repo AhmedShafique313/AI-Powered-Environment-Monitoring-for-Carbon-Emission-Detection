38531f2969a1b989fee516ef5fcab44e199a086d
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2024-11-19 13:28:53 +0500

    dataset is cleaned and now it is ready for preprocessing step

--- dataset_cleaning/clean_32.py
+++ dataset_cleaning/clean_32.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv(r'C:\Users\Ahmed Shafique\Documents\Projects\FYP\Datasets\latitude_32.csv')
-print(df.head())
+# print(df.head())
+df = df.drop_duplicates()
+df = df.dropna()
+df['time'] = pd.to_datetime(df['time'])
+print(df.info())
