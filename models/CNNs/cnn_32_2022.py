a67031c919556ca69996df01bcca741d902fbe14
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2024-12-07 20:00:51 +0500

    Because I am working on 2022 dataset

---
+++ models/CNNs/cnn_32_2022.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\Ahmed Shafique\Documents\Projects\AI Powered Enviromental Monitoring for Carbon Detection\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\Finalized dataset\dataset2022.csv')
+print(df.head())
