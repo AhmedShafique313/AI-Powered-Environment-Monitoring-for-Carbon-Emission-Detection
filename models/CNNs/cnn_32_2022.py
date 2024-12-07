0af505c7b49f216b9eb483746fa4e2fd6797d531
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2024-12-07 21:18:58 +0500

    cnn applied

--- models/CNNs/cnn_32_2022.py
+++ models/CNNs/cnn_32_2022.py
@@ -1,33 +1,28 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
-train_df = pd.read_csv(r'C:\Users\Ahmed Shafique\Documents\Projects\AI Powered Enviromental Monitoring for Carbon Detection\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\Finalized dataset\dataset2022.csv')
-X_train = train_df.drop(['avg_temp', 'time', 'year', 'month', 'hour'], axis=1)
+train_df = pd.read_csv(r'/content/dataset2022.csv')
+X_train = train_df[['no', 'no2', 'o3', 'so2', 'ch4']]
 y_train = train_df['co']
-
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
-
 X_train_reshaped = X_train_scaled.reshape(X_train_scaled.shape[0], X_train_scaled.shape[1], 1)
-
 model = Sequential()
 model.add(Conv1D(64, kernel_size=2, activation='relu', input_shape=(X_train_reshaped.shape[1], 1)))
-model.add(MaxPooling1D(pool_size=2))
+model.add(MaxPooling1D(pool_size=1))
 model.add(Conv1D(32, kernel_size=2, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(1))
 
-model.compile(optimizer=Adam(lr=0.001), loss='mean_squared_error')
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
 early_stopping = EarlyStopping(monitor='loss', patience=5, min_delta=0.001)
 history = model.fit(X_train_reshaped, y_train, epochs=50, batch_size=32, callbacks=[early_stopping])
 
