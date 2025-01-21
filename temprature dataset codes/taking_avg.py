8fb39488c834c20509147730f646c2c5437d80bc
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-21 19:49:55 +0500

    Average temprature calulated of 2022 dataset

--- temprature dataset codes/taking_avg.py
+++ temprature dataset codes/taking_avg.py
@@ -3,10 +3,10 @@ import pandas as pd
 csv_file_path = r'C:\Users\Ahmeds Gaming Laptop\Documents\Projects\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\22temprature_dataset.csv'  
 df = pd.read_csv(csv_file_path)
 
-column1 = 'MFeb'  
-column2 = 'mFeb'  
+column1 = 'MDec'  
+column2 = 'mDec'  
 
-df['Avg_Feb'] = df[[column1, column2]].mean(axis=1)
+df['Avg_Dec'] = df[[column1, column2]].mean(axis=1)
 
 df.to_csv(csv_file_path, index=False)
 
