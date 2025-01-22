eeafdf1b86b6de8c124ff52c536820a0e42e2c12
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-22 15:02:03 +0500

    Average calculated of 2023 dataset

--- temprature dataset codes/taking_avg.py
+++ temprature dataset codes/taking_avg.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-csv_file_path = r'C:\Users\Ahmeds Gaming Laptop\Documents\Projects\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\22temprature_dataset.csv'  
+csv_file_path = r'C:\Users\Ahmeds Gaming Laptop\Documents\Projects\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\23temperature_dataset.csv'  
 df = pd.read_csv(csv_file_path)
 
-column1 = 'MDec'  
-column2 = 'mDec'  
+column1 = 'Dec_Old'  
+column2 = 'Dec_New'  
 
 df['Avg_Dec'] = df[[column1, column2]].mean(axis=1)
 
