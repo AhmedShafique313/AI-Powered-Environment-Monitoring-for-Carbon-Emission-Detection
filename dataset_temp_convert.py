c92da5a67704efac624181c1d5a615d1db8bcd03
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-22 14:44:32 +0500

    maximum temprature dataset added

--- dataset_temp_convert.py
+++ dataset_temp_convert.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Load the text file
-file_path = r'C:\Users\Ahmeds Gaming Laptop\Documents\Projects\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\Datasets\22_year_temp.txt'  # Path to your saved text file
+file_path = r'C:\Users\Ahmeds Gaming Laptop\Documents\Projects\AI-Powered-Environment-Monitoring-for-Carbon-Emission-Detection\Datasets\23max_temp.txt'  # Path to your saved text file
 df = pd.read_csv(file_path, delim_whitespace=True)
 
 # Save as a CSV file
-output_csv_path = '22_temperature_dataset.csv'
+output_csv_path = '23_temperature_dataset.csv'
 df.to_csv(output_csv_path, index=False)
 print(f"CSV file saved as {output_csv_path}")
