ab00ffcce705d4e54b0f54754db17187b91704c2
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-01-30 21:01:56 +0500

    dataloaded and splitted

---
+++ Neural Network/split.py
@@ -0,0 +1,9 @@
+from dataload import X, y_pm25, y_temp
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train_pm25, y_test_pm25, y_train_temp, y_test_temp = train_test_split(X_scaled, y_pm25, y_temp, test_size=0.2, random_state=42)
+
